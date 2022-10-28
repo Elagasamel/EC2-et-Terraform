8be011873c9fa8974464fe1de8f74f6f54fc7723
Elagas Amel <lagas.amel@gmail.com>
2022-10-28 10:01:17 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,10 +2,10 @@
 resource "aws_instance" "web" {
   ami             = "ami-08cfb7b19d5cd546d"
   instance_type   = "t2.micro"
-  key_name        = "test-amine-berguiga"
+  key_name        = "amel"
   user_data       = file("init.sh")
   security_groups = [aws_security_group.allow_tls.name]
   tags = {
-    Name = "devops-b-tounsi"
+    Name = "amel elagas"
   }
 }
